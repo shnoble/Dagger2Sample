5fd462f2ae9eeb744aab1f1c96ab0408cede8139
홍석현/TOAST클라이언트개발팀/NE <shhong@nhnent.com>
2019-04-12 11:13:42 +0900

    Init commit.

---
+++ app/src/main/java/com/daya/kotlin/dagger2/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daya.kotlin.dagger2.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
